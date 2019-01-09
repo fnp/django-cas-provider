dd16afa78c011b3f6cd0b2c1860fa29a5494481f
Radek Czajka <rczajka@rczajka.pl>
2019-01-09 12:17:03 +0100

    Just upgrade versions.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description=read('README.rst'),
     zip_safe=False,
     install_requires=[
-        'Django>=1.5,<1.9',
+        'Django>=1.5,<2.2',
         'lxml',
         ],
     classifiers = [
